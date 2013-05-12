ebcdb93c695acf2b405db61c60ca4297b38edd6f
kadaj683 <vlad683@gmail.com>
2013-05-12 21:45:55 +0300

    Stats

--- recognition.js
+++ recognition.js
@@ -2,6 +2,8 @@ var canvas;
 var context;
 var clicking=false;
 
+var chart;
+
 $(function(){
 	canvas = document.getElementById("input");
 	context = canvas.getContext("2d");
@@ -10,7 +12,7 @@ $(function(){
 		var x = e.pageX - $(this).offset().left ;
 		var y = e.pageY - $(this).offset().top ;
 		move(x,y);
-		$('#debug').html("moving X: " + x + " Y: " + y);
+		//$('#debug').html("moving X: " + x + " Y: " + y);
 	});
 	$("#input").mousedown(function(e){
 		var x = e.pageX - $(this).offset().left;
@@ -27,20 +29,143 @@ $(function(){
 		$('#debug').html("Done Clicking");
 	});
 
+	makechart();
+
 })
 
+
+var pt;
+var angles=[];
 function press(x,y){
 	context.strokeStyle = "black";
 	context.lineWidth = 3;
 	context.beginPath();
 	context.moveTo(x,y);
 
+	angles=[];
+	pt={x:x,y:y};
 }
+function distance(p1,p2){
+	var width = p2.x-p1.x;
+	var height = p2.y-p1.y;
+	return Math.sqrt(width*width+height*height);
+}
+
+function diff(a){
+	var res=[];
+	var p0={x:0,y:a[0]}
+	for(var x=1;x<a.length;x++){
+		var p={x:x,y:a[x]};
+		res.push(((p.y-p0.y)/(p.x-p0.x)));
+		p0=p;
+	}
+	return res;
+
+	
+}
+function diff_angle(a){
+	var res=[];
+	var p0={x:0,y:a[0]}
+	for(var x=1;x<a.length;x++){
+		var p={x:x,y:a[x]};
+		res.push(Math.atan2((p.y-p0.y),(p.x-p0.x)));
+		p0=p;
+	}
+	return res;	
+}
+function rev(a){
+	var res=[];
+	var p0={x:0,y:a[0]}
+	for(var x=1;x<a.length;x++){
+		var p={x:x,y:a[x]};
+		res.push(1/Math.abs(p.y-p0.y));
+		p0=p;
+	}
+	return res;	
+}
+
 function move(x,y){
 	context.lineTo(x,y);
 	context.stroke();
+
+	var pt1={x:x,y:y};
+	var dist = distance(pt1,pt);
+	//if(dist<10) return;
+	var width=pt1.x-pt.x;
+	var height=pt1.y-pt.y;
+	var angle=Math.atan2(height,width)/Math.PI*180;
+
+	//context.beginPath();
+	//context.arc(x, y, 3, 0, 2 * Math.PI, true);
+	//context.fill();
+	context.fillStyle = '#f00';
+	context.fillText(angles.length,x+20,y);
+
+	pt=pt1;
+	angles.push(angle);
+	$('#debug').html(angle);
 }
 function release(x,y){
 	context.lineTo(x,y);
 	context.stroke();
+
+	
+	chart.addSeries({data:rev(diff(diff_angle(angles)))});
+
+}
+
+
+
+function makechart()
+{
+	var data=[1,2,3,4,-5];
+   	chart=$('#chart').highcharts({
+        chart: {
+            zoomType: 'x',
+            spacingRight: 20
+        },
+        xAxis: {
+        	type: "linear",
+            title: {
+                text: null
+            }
+        },
+        yAxis: {
+            title: {
+                text: null
+            }
+        },
+        plotOptions: {
+            area: {
+                fillColor: {
+                    linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1},
+                    stops: [
+                        [0, Highcharts.getOptions().colors[0]],
+                        [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                    ]
+                },
+                lineWidth: 1,
+                marker: {
+                    enabled: false
+                },
+                shadow: false,
+                states: {
+                    hover: {
+                        lineWidth: 1
+                    }
+                },
+                threshold: null
+            }
+        },
+
+        /*series: [{
+            type: 'area',
+            name: null,
+            pointInterval: 1,
+            pointStart: 0,
+            data: data
+        }]*/
+    });
+    chart=chart.highcharts();
+
 }
